8ff470a2df010e345bd36a43e203d0a647c52471
Valerie Chatelet <vlrchtlt@gmail.com>
2018-01-08 19:49:57 +0100

    premier commit

---
+++ app.rb
@@ -0,0 +1,14 @@
+require "sinatra"
+require "http"
+require 'json'
+
+get '/'  do
+  address = URI.escape(params["address"])
+
+  respons = HTTP.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{address}").body.to_s
+  respons = JSON.parse(respons)
+  results = respons[results].inspect
+  #lgn = respons[geometry][location][lgn]
+  #p lat
+
+end
